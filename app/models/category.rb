5a9ad7778e72694047a990dba22f473191723fa0
Kehinde Onifade <onifkay@gmail.com>
2022-12-20 23:48:38 +0100

    Add validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,7 @@ class Category < ApplicationRecord
 
   has_many :category_transactions, dependent: :destroy
   has_many :transactions, through: :category_transactions
+
+  validates :name, presence: true
+  validates :icon, presence: true
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,4 +3,6 @@ class Transaction < ApplicationRecord
 
   has_many :category_transactions, dependent: :destroy
   has_many :categories, through: :category_transactions
+
+  validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
