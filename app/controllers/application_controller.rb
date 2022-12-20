d2a57bff35b2c762bbc648c745feb0cc60077dbd
Kehinde Onifade <onifkay@gmail.com>
2022-12-21 00:13:53 +0100

    Add splash screens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate!
+
+  def index; end
+
+  private
+
+  def authenticate!
+    redirect_to categories_path if user_signed_in?
+  end
 end
