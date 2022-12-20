7b9c1eb203e367438049df2db7f727f790e2ff36
Kehinde Onifade <onifkay@gmail.com>
2022-12-20 23:19:44 +0100

    create transaction migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_20_220546) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_20_221218) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_20_220546) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.string "name"
+    t.integer "amount"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_transactions_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -47,4 +56,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_20_220546) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "transactions", "users", column: "author_id"
 end
