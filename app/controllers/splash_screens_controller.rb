56d0a2b22412165834daa9c6525eb9d703c7eb47
Kehinde Onifade <onifkay@gmail.com>
2022-12-21 10:36:07 +0100

    Fix linters

--- app/controllers/splash_screens_controller.rb
+++ app/controllers/splash_screens_controller.rb
@@ -1,9 +1,8 @@
 class SplashScreensController < ApplicationController
-    before_action :authenticate!
-    def index
-    end
+  before_action :authenticate!
+  def index; end
 
-    private
+  private
 
   def authenticate!
     redirect_to categories_path if user_signed_in?
