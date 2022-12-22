dab65380932fd05b77ecc64793c31569b8aa982b
Kehinde Onifade <onifkay@gmail.com>
2022-12-22 23:16:22 +0100

    update

--- spec/integration/login_spec.rb
+++ spec/integration/login_spec.rb
@@ -4,9 +4,9 @@ RSpec.feature 'Logins', type: :feature do
   background { visit new_user_registration_path }
 
   scenario 'with valid information' do
-    fill_in 'user[name]', with: 'victor'
-    fill_in 'Email', with: 'okorojiebube2@gmail.com'
-    fill_in 'Password', with: 'victor12345'
+    fill_in 'user[name]', with: 'kenny'
+    fill_in 'Email', with: 'rideonone@gmail.com'
+    fill_in 'Password', with: 'kenny12345'
 
     click_button 'Next'
     expect(page).to have_content('')
@@ -15,7 +15,7 @@ RSpec.feature 'Logins', type: :feature do
   scenario 'with invalid information' do
     fill_in 'user[name]', with: ''
     fill_in 'Email', with: 'okorojiebubue@gmail.com'
-    fill_in 'Password', with: 'victor12345'
+    fill_in 'Password', with: 'kenny12345'
 
     click_button 'Next'
     expect(page).to have_content('')
